1b25ac9840b9dbfbeee849ba8c411d72f41b3f39
nndieu <nndieu88@gmail.com>
2019-09-12 20:19:01 +0700

    SS4

---
+++ src/main/java/TestTriangle.java
@@ -0,0 +1,6 @@
+public class TestTriangle {
+    public static void main(String[] args) {
+        System.out.println(new Triangle());
+        System.out.println(new Triangle(12.0, 8.0, 9.0));
+    }
+}
